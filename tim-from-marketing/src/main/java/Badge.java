605a80a6d1f12fc18b3d7d86af29c68de7910301
J Alexis TH <colores.2808.8000@gmail.com>
2026-09-22 12:25:46 -0600

    Second exercise: tim-from-marketing

---
+++ tim-from-marketing/src/main/java/Badge.java
@@ -0,0 +1,12 @@
+class Badge {
+    public String print(Integer id, String name, String department) {
+        if ( department == null ) {
+            department = "owner";
+        }
+        if ( id == null ) {
+            return String.format("%s - %s",name, department.toUpperCase());
+        }
+        return String.format("[%d] - %s - %s", id, name, department.toUpperCase());
+    }
+}
+
